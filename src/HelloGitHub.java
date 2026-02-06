30f2ead9beb08187c4db0dce326a9d780a88098d
Devika <nanav24d@mtholyoke.edu>
2026-02-06 12:38:13 -0500

    2nd version of Hello, world.

--- src/HelloGitHub.java
+++ src/HelloGitHub.java
@@ -3,7 +3,8 @@ public class HelloGitHub {
 
     public static void main(String[] args) {
         System.out.println("Hello, world.");
-
+        System,out.println("hallo, wereld.");
+        System.out.println("Hola, mundo.");
     }
 
 }
